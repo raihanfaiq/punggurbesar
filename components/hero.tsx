e07211bf5c97ff56147d3140bd8884b64d543005
raihanfaiq <raihanfaiq20@gmail.com>
2023-07-23 09:11:46 +0700

    initial hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,15 +1,26 @@
-import VideoThumb from '@/public/images/hero-image.png'
-import ModalVideo from '@/components/modal-video'
+import VideoThumb from '@/public/images/hero-image.png';
+import ModalVideo from '@/components/modal-video';
+import { HeroImage } from '@/public/images/HeroImage';
 
 export default function Hero() {
   return (
-    <section className="relative">
-
+    <section className="relative bg-[#28400D]">
       {/* Illustration behind hero content */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
-        <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+      <div
+        className="absolute bottom-0 transform -translate-x-1/2 pointer-events-none left-1/2 -z-1"
+        aria-hidden="true">
+        <svg
+          width="1360"
+          height="578"
+          viewBox="0 0 1360 578"
+          xmlns="http://www.w3.org/2000/svg">
           <defs>
-            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+            <linearGradient
+              x1="50%"
+              y1="0%"
+              x2="50%"
+              y2="100%"
+              id="illustration-01">
               <stop stopColor="#FFF" offset="0%" />
               <stop stopColor="#EAEAEA" offset="77.402%" />
               <stop stopColor="#DFDFDF" offset="100%" />
@@ -22,40 +33,29 @@ export default function Hero() {
         </svg>
       </div>
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
-        {/* Hero content */}
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
-          {/* Section header */}
-          <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Make your website <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">wonderful</span></h1>
-            <div className="max-w-3xl mx-auto">
-              <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
-              <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
-                <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
-                </div>
-                <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-                </div>
-              </div>
+      {/* Hero content */}
+      <div className="pt-6 pl-8">
+        {/* Section header */}
+        <div className="bg-#[#28400D] relative">
+          <div className="flex">
+            <div className="relative w-3/5 pb-4 space-y-8 ml-14 lg:pt-44 ">
+              <h1 className="text-[64px] leading-none text-white">
+                Memelihara, Memanen, <br /> & Menyebarkan
+              </h1>
+              <p className="text-[16px] text-gray-300 lg:w-11/12">
+                Punggur Besar merupakan Desa Perkebunan dengan hasil bumi yang
+                melimpah yang dimanfaatkan secara mentah maupun dijadikan
+                sebagai bahan dasar pembuatan berbagai macam olahan
+              </p>
+              <p className="text-[#E8BB15]">JELAJAHI DESA</p>
+            </div>
+            <div className="w-2/5 h-full border-l border-1 mt-14">
+              {/* Hero image */}
+              <HeroImage />
             </div>
           </div>
-
-          {/* Hero image */}
-          <ModalVideo
-            thumb={VideoThumb}
-            thumbWidth={768}
-            thumbHeight={432}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/video.mp4"
-            videoWidth={1920}
-            videoHeight={1080} />
-
         </div>
-
       </div>
     </section>
-  )
+  );
 }
