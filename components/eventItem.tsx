16084d395066defb319ab8773cfb5ef4956b2c1f
raihanfaiq <raihanfaiq20@gmail.com>
2023-08-08 06:14:08 +0700

    responsive better

--- components/eventItem.tsx
+++ components/eventItem.tsx
@@ -23,10 +23,10 @@ export default function eventItem({ event, index, activeEvent, setActiveEvent }:
       id="event"
     >
       <div className="relative w-16 h-16 flex-2">
-        <p className="absolute font-[AbrilFatface]  text-[32px] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-[#82C75C] z-10">0{index}</p>
+        <p className="absolute font-[AbrilFatface] text-[32px] font-bold text-[#82C75C] z-10">0{index}</p>
       </div>
-      <div className="flex flex-col items-start justify-start flex-1 text-white">
-        <h3 className={`font-bold font-[AbrilFatFace] text-4xl 2xl:text-5xl ${index == activeEvent ? "translate-y-0" : "-translate-y-2"} group-hover:translate-y-0 transition duration-300`}>{event["title"]}</h3>
+      <div className="flex flex-col items-start justify-start text-white">
+        <h3 className={`font-bold font-[AbrilFatFace] text-2xl xl:text-4xl`}>{event["title"]}</h3>
         <div className={`${index == activeEvent ? "h-52 opacity-100" : "h-0 opacity-0"} group-hover:h-52 group-hover:opacity-100 transition-all duration-300 ease-out`}>
           <p className="mt-4 text-md 2xl:text-lg font-[JosefinSans] text-[#82C75C]">{event["description"]}</p>
         </div>
--- components/features.tsx
+++ components/features.tsx
@@ -9,27 +9,27 @@ import FeaturesElement from "@/public/images/features-element.png";
 export default function Features() {
   return (
     <div className="bg-[#F1EAE2]">
-      <div className="container mx-auto p-10 columns-2 md:columns-2 lg:columns-4">
+      <div className="container p-10 mx-auto columns-2 md:columns-2 lg:columns-4">
         <div className="flex text-[#28400D]">
-          <h1 className="text-[48px] font-[AbrilFatface] font-bold">28</h1>
+          <h1 className="text-[24px] lg:text-[48px] font-[AbrilFatface] font-bold">28</h1>
           <h1 className="mt-2 ml-4 text-[16px] font-[JosefinSans]">
             Komoditas <br /> Tani
           </h1>
         </div>
         <div className="flex text-[#28400D]">
-          <h1 className="text-[48px] font-[AbrilFatface] font-bold">100k</h1>
+          <h1 className="text-[24px] lg:text-[48px] font-[AbrilFatface] font-bold">100k</h1>
           <h1 className="mt-2 ml-4 text-[16px] font-[JosefinSans]">
             Rata-Rata Panenan <br /> per Komoditas
           </h1>
         </div>
         <div className="flex text-[#28400D]">
-          <h1 className="text-[48px] font-[AbrilFatface] font-bold">12</h1>
+          <h1 className="text-[24px] lg:text-[48px] font-[AbrilFatface] font-bold">12</h1>
           <h1 className="mt-2 ml-4 text-[16px] font-[JosefinSans]">
             Macam <br /> Komoditas
           </h1>
         </div>
         <div className="flex text-[#28400D]">
-          <h1 className="text-[48px] font-[AbrilFatface] font-bold ">10k+</h1>
+          <h1 className="text-[24px] lg:text-[48px] font-[AbrilFatface] font-bold ">10k+</h1>
           <h1 className="mt-2 ml-4 text-[16px] font-[JosefinSans]">
             Hektar Lahan <br /> Perkebunan
           </h1>
